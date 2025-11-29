3fee814589375fdcafed283b45692c159c805391
Tural Isayev <isayev.tural.official@gmail.com>
2025-11-29 14:26:29 +0000

    c

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,9 @@
 
 for i in range(0, 10):
     for j in range(i+1, 10):
-        print("{}{}".format(i, j), end=", ")
+        if "{}{}".format(i, j) != "89":
+            print("{}{}".format(i, j), end=", ")
+
+        else:
+            print("89")
+
