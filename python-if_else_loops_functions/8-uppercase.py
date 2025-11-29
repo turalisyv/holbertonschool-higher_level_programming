629a67a3f23f54ae1b88b95353e52ba61a801bd2
Tural Isayev <isayev.tural.official@gmail.com>
2025-11-29 14:53:24 +0000

    c

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,5 +6,8 @@ def uppercase(str):
         if i == " ":
             new_str = new_str + " "
             continue
+        if ord(i) >= ord('A') and ord(i) <= ord('Z'):
+            new_str = new_str + i
+            continue
         new_str = new_str + chr(ord(i)-32)
     print("{}".format(new_str))
