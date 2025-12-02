5acd062490b1c87d22360655e1a45c49d27e58fd
Tural Isayev <isayev.tural.official@gmail.com>
2025-12-02 14:03:58 +0000

    c

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(0, n):
         for j in range(0, n):
             if j == n - 1:
-                print("{}".format(matrix[i][j]), end="")
+                print("{:d}".format(matrix[i][j]), end="")
                 break
-            print("{} ".format(matrix[i][j]), end="")
+            print("{:d} ".format(matrix[i][j]), end="")
         print()
