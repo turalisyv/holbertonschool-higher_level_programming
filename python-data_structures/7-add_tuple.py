57e0ca158b0ff798034ea983d17a531f6da9d93f
Tural Isayev <isayev.tural.official@gmail.com>
2025-12-02 14:29:35 +0000

    c

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    total = list(tuple_a)
-    for i in range(len(tuple_b)):
-        if i < len(total):
-            total[i] += tuple_b[i]
-
+    total = [0, 0]
+    for i in range(0, 2):
+        if i >= len(tuple_a):
+            total[i] = 0 + tuple_b[i]
+        if i >= len(tuple_b):
+            total[i] = tuple_a[i] + 0
         else:
-            total.append(tuple_b[i])
+            total[i] = tuple_a[i] + tuple_b[i]
 
     return tuple(total)
