e5a6de122aa82ad0943f06de6bd989e86729d563
Tural Isayev <isayev.tural.official@gmail.com>
2025-12-02 12:02:20 +0000

    c

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-for i in range(len(sys.argv) - 1):
-    print("{}: {}".format(i+1, sys.argv[i+1]))
+    if len(sys.argv) == 1:
+        print("0 argument.")
+
+    else:
+        print("{} arguments.".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv) - 1):
+            print("{}: {}".format(i+1, sys.argv[i+1]))
