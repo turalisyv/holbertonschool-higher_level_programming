0862a0be07930c4316ba37dad6e37d4409c853d9
Tural Isayev <isayev.tural.official@gmail.com>
2025-12-03 12:57:51 +0000

    c

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    new_dictionary = a_dictionary.copy()
     try:
-        new_dictionary[key] = value
+        a_dictionary[key] = value
 
     except:
-        new_dictionary.update({key: value})
+        a_dictionary.update({key: value})
 
-    return new_dictionary
+    return a_dictionary
