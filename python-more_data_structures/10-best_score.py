74adce727df62fe7fe3dc138b788dcbaa6dc862f
Tural Isayev <isayev.tural.official@gmail.com>
2025-12-03 15:59:48 +0000

    c

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None or a_dictionary == {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
 
     if 'John' in a_dictionary:
