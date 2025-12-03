9a56fe70514272b70046c4212d492ce07efe14d2
Tural Isayev <isayev.tural.official@gmail.com>
2025-12-03 15:41:14 +0000

    c

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if type(roman_string) is not str:
+        return None
+
     numbers  = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,"M": 1000}
     total = 0
     tmp = roman_string[0]
 
-    for i in range(1, len(roman_string)): # C X X I V
+    for i in range(1, len(roman_string)):
         if numbers[roman_string[i]] <= numbers[roman_string[i-1]]:
             total += numbers[roman_string[i-1]]
         else:
