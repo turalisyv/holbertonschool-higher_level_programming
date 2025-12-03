e4643d8224a22e03743ff9c1446a0d3ca240cf3f
Tural Isayev <isayev.tural.official@gmail.com>
2025-12-03 13:07:23 +0000

    c

---
+++ python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    for key, value in a_dictionary.items():
+        if type(value) is int:
+            a_dictionary[key] *= 2
+
+    return a_dictionary
