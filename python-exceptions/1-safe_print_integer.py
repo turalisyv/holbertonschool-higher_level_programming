59002c346676f2c8ef76c0c89d9dd8d7f0c61552
Your Name <you@example.com>
2025-12-05 13:20:48 +0000

    c

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,8 @@ def safe_print_integer(value):
         print("{:d}".format(value))
         return True
 
-    except ValueError or TypeError:
+    except ValueError:
+	return False
+
+    except TypeError:
         return False
