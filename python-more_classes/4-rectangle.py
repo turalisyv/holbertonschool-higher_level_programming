b13f73d4525a1649367efcfffde37175e39c35e9
Your Name <you@example.com>
2025-12-10 07:35:47 +0000

    c

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+'''
+My module document
+'''
+
+
+class Rectangle():
+    '''
+    My class document
+    '''
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        res = ""
+        if self.__height == 0 or self.__width == 0:
+            return res
+
+        for i in range(self.__height):
+            for j in range(self.__width):
+                res = res + "#"
+
+            if i < self.__height - 1:
+                res = res + "\n"
+
+        return res
+    
+    def __repr__(self):
+        return str("Rectangle({}, {})".format(self.__width, self.__height))
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+
+        else:
+            return 2 * (self.__height + self.__width)
