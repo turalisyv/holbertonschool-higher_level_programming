7a17a0e5299e6188d2912e11e3a208893630eb69
Your Name <you@example.com>
2025-12-12 19:17:42 +0000

    c

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+My module document
+'''
+
+
+def write_file(filename="", text=""):
+    '''
+    My function document
+    '''
+    with open(file=filename, mode="a", encoding="utf-8") as f:
+        f.write(text)
+        return len(text)
