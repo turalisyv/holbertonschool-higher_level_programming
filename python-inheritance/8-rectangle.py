6b34bf1fa760729796380bd34d71ce162ca2566e
Your Name <you@example.com>
2025-12-12 11:01:31 +0000

    c

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,20 +4,7 @@ My module document
 '''
 
 
-class BaseGeometry():
-    '''
-    My class document
-    '''
-    def integer_validator(self, name, value):
-        if not type(value) is int:
-            raise TypeError("{} must be an integer".format(name))
-
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     '''
