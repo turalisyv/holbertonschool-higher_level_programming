fa8049df7c9b1e55a89a7cd21920f67dd914af1a
Your Name <you@example.com>
2025-12-12 18:39:54 +0000

    c

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ def read_file(filename=""):
     '''
     My function document
     '''
-    with open(file=filename, mode="r") as f:
+    with open(file=filename, mode="r", encoding="utf-8") as f:
         print(f.read())
