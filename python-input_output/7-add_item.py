ba568ae1b08df9fac1ceeffa2d9c438ca51e064a
Your Name <you@example.com>
2025-12-12 20:16:57 +0000

    c

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 
 if __name__=="__main__":
+    '''
+    And God said "let there run code!"
+    '''
     import sys
     import json
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+    try:
+        data = load_from_json_file("add_item.json")
 
-    data = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        with open("add_item.json", "w") as f:
+            f.close()
+        data = []
+        save_to_json_file(data, "add_item.json")
 
     for i in range(1, len(sys.argv)):
         data.append(sys.argv[i])
