99509ae7485537a959a6935272bd19682a92cd57
Your Name <you@example.com>
2025-12-12 09:44:57 +0000

    c

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+My module document
+'''
+
+
+class BaseGeometry():
+    '''
+    My class document
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
