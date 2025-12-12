74fee811eb8d273122de8fe00987e586948b6005
Your Name <you@example.com>
2025-12-12 11:19:30 +0000

    c

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -22,4 +22,4 @@ class Square(Rectangle):
         return self.__size * self.__size
 
     def __str__(self):
-        return "[Square] {}/{}".format(self.__width, self.__height)
+        return "[Square] {}/{}".format(self.__size, self.__size)
