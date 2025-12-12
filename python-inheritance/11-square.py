a7ac4eacd35bd778d2172bee3bad4daac313252d
Your Name <you@example.com>
2025-12-12 11:17:55 +0000

    c

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+My module document
+'''
+
+
+Rectangle = __import__('script').Rectangle
+
+
+class Square(Rectangle):
+    '''
+    My class document
+    '''
+    def __init__(self, size):
+        Rectangle.integer_validator(self, "width", size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__width, self.__height)
