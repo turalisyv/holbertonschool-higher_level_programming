faef1facb276781762670d0ed889bc329a6e6193
Your Name <you@example.com>
2025-12-13 11:46:18 +0000

    c

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+My module document
+'''
+
+
+def class_to_json(obj):
+    '''
+    My function document
+    '''
+    return {
+        "name": obj.name,
+        "number": obj.number
+    }
