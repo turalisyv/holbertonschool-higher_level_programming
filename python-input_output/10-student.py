bc35ccee4bf7e4d17c4e45beab45227850b78472
Your Name <you@example.com>
2025-12-13 12:28:24 +0000

    c

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+My module document
+"""
+
+class Student:
+    """
+    My class document
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        if attr == None:
+            return self.__dict__
+
+        dict = {}
+        for i in attr:
+            try:
+                dict.update({i: self.__dict__[i]})
+
+            except:
+                continue
+
+        return dict
