cfe614f33f5794ab339c5cec71ee39e024720d5e
Your Name <you@example.com>
2025-12-15 07:12:12 +0000

    c

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -14,12 +14,15 @@ def convert_csv_to_json(data):
 
     json_data = []
 
-    with open(data, mode='r', newline='') as file:
+    try:
+        with open(data, mode='r', newline='') as file:
+            csv = csv.reader(file)
+            csv = list(csv)
+            for i in csv[1:]:
+                json_data.append(dict(zip(csv[0], i)))
 
-        csv = csv.reader(file)
-        csv = list(csv)
-        for i in csv[1:]:
-            json_data.append(dict(zip(csv[0], i)))
+        with open("data.json", mode="w") as f:
+            json.dump(json_data, f, indent=4)
 
-    with open("data.json", mode="w") as f:
-        json.dump(json_data, f, indent=4)
+    except Exception:
+        return False
