685b8203e0e8c358c7ca596023d9bffe64551d5a
Your Name <you@example.com>
2025-12-15 07:36:09 +0000

    c

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,19 +1,28 @@
-#!/usr/bin/python3
-'''
-My module document
-'''
-
-
-def convert_csv_to_json(data):
+def convert_csv_to_json(csv_file_path):
     '''
     My function document
     '''
 
     import csv
     import json
+    import os
 
-    with open(data, mode='r', newline='', encoding='utf-8') as csvfile:
-        data = list(csv.DictReader(csvfile))
+    data = []
+    try:
+        with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                data.append(row)
 
-    with open('data.json', mode='w', encoding='utf-8') as jsonfile:
-        json.dump(data, jsonfile, indent=4)
+    except FileNotFoundError:
+        return False
+
+    except Exception:
+        return False
+
+    try:
+        with open(json_file_path, mode='w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=4)
+
+    except Exception:
+        return False
