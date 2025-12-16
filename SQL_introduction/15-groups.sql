46d4a56833c2fa43b08d4b474bfdb1ba479233c8
Your Name <you@example.com>
2025-12-16 11:48:57 +0000

    c

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Number by score --
 
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC
