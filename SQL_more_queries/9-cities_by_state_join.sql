cceacc1d254865f962ccc816e15ddd8485fc5822
Your Name <you@example.com>
2025-12-21 02:06:57 +0000

    c

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Cities by States --
 
-SELECT cities.id, cities.name, states.name FROM cities, states
+SELECT cities.id, cities.name, states.name FROM cities
 JOIN states ON states.id = cities.state_id
 ORDER BY cities.id
