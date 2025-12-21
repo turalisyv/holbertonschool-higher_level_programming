8be2f8958fc57463f143318333f79c6ddd73c4ce
Your Name <you@example.com>
2025-12-21 02:37:28 +0000

    c

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- Genre ID for all shows --
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
-RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_show.genres.show_id IS NULL
- ORDER BY tv_shows.title, tv_show_genres.genre_id
+RIGHT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id
