eb1ad566e164c79a312de1209846ae822a9e8b44
Your Name <you@example.com>
2025-12-21 02:47:12 +0000

    c

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Number of shows by genre --
 
-SELECT tv_genres.name, COUNT(*) AS number_of_shows FROM tv_genres
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
