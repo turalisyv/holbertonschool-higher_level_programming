068533adefd4ff1e9032d2770673b9c4e6d02b85
Your Name <you@example.com>
2025-12-21 02:01:03 +0000

    c

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Cities of California --
 
-SELECT id, name FROM cities WHERE (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id
