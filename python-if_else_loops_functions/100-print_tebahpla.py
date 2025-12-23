89b6cd645d57f7047bb6bf0bdba5fa4db63f63be
Your Name <you@example.com>
2025-12-23 12:51:36 +0000

    c

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 
 for i in range(13):
     print("{}".format(chr(ord('z') - (i * 2))), end="")
-    print("".format(chr(ord('z') - (i * 2 + 1)).upper()), end="")
+    print("{}".format(chr(ord('z') - (i * 2 + 1)).upper()), end="")
