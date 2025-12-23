dd601cbd32c62dfe9703676f453e6254e7f55048
Your Name <you@example.com>
2025-12-23 12:49:16 +0000

    c

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(13):
-    print(chr(ord('z') - (i * 2)), end="")
-    print(chr(ord('z') - (i * 2 + 1)).upper(), end="")
+    print("{}".format(chr(ord('z') - (i * 2))), end="")
+    print("".format(chr(ord('z') - (i * 2 + 1)).upper()), end="")
